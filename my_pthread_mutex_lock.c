e8dedb88d9298e0ffaecc78fa7ba54a8b74c5359
Yichen Yue <yue.yichen1992@gmail.com>
2016-10-21 21:46:21 -0400

    Add yield()

--- my_pthread_mutex_lock.c
+++ my_pthread_mutex_lock.c
@@ -1,3 +1,4 @@
+#include "my_pthread_t.h"
 #include "my_pthread_mutex_t.h"
 
 int my_pthread_mutex_lock(my_pthread_mutex_t *mutex){
@@ -6,6 +7,6 @@ int my_pthread_mutex_lock(my_pthread_mutex_t *mutex){
 		return EINVAL;
 	}
 	while(__sync_lock_test_and_set(&(mutex->flag), 1))
-		;
+	my_pthread_yield();
 	return 0;
 }
